3a38b162992d2f97ca71da76e6d1586ad1c35e77
Elvis Lopes <evs.lopes@gmail.com>
2021-09-21 20:29:49 -0300

    Implementando stores

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,16 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import timesModules from "@/store/modules/timesModules";
 import jogadoresModules from "@/store/modules/jogadoresModules";
+import partidasModules from "@/store/modules/partidasModules";
+import golsModules from "@/store/modules/golsModules";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     modules: {
         timesModules,
-        jogadoresModules
+        jogadoresModules,
+        partidasModules,
+        golsModules
     }
 })
